552d945351a559c0ab56378ce84f9c4763551c38
Haohao Tian <llitfkitfk@192-168-1-117.tpgi.com.au>
2013-04-17 23:33:58 +1000

    second upload

---
+++ MediatorPattern/src/MediatorPatternDemo.java
@@ -0,0 +1,8 @@
+
+public class MediatorPatternDemo {
+	public static void main(String[] args) {
+		User user = new User("Paul");
+		User user2 = new User("klsjd");
+		user.sendMessage("hello");
+	}
+}
---
+++ MediatorPattern/src/User.java
@@ -0,0 +1,24 @@
+
+public class User {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public User(String name) {
+		super();
+		this.name = name;
+	}
+
+	public void sendMessage(String string) {
+		// TODO Auto-generated method stub
+		ChatRoom.showMessage(this, string);
+	}
+	
+	
+}
